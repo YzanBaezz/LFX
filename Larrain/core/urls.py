d874cc77567eac9929734becf0dc9fbe5afad114
YzanBaezz <yz.baez@duocuc.cl>
2022-07-07 19:39:33 -0400

    cambios-login-registro.html

--- Larrain/core/urls.py
+++ Larrain/core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, registro
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', index, name="index"),
+
+    path('login/', LoginView.as_view(template_name='core/login.html'), name="login"),
+    path('logout/', LogoutView.as_view(template_name='core/logout.html'), name="logout"),
+    path('registro/', registro, name="registro"),
 ]
--- Larrain/core/views.py
+++ Larrain/core/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import views
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'core/index.html')
+
+def login(request):
+
+    return render(request, 'core/login.html')
+
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'Usuario {username} creado')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    contexto = {'form': form}
+    return render(request, 'core/registro.html', contexto)
+
+
+def logout(request):
+
+    return render(request, 'core/logout.html')
