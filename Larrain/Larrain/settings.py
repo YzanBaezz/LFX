298e085a8f0461e26b71f37ca97137a1be43a992
Aaron Perez <anib.perezm@duocuc.cl>
2022-11-23 02:30:01 -0300

    2.0.4

--- Larrain/Larrain/settings.py
+++ Larrain/Larrain/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Larrain.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xe',
+        'NAME': '127.0.0.1:1521/orcl',
         'USER': 'ProyectoSoftware',
         'PASSWORD': '123456',
         'TEST': {
